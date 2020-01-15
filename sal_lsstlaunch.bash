4983df6e106e8298fd2aeaeec8dfb0ca3cb0f1fc
adam <athornton@gmail.com>
2020-01-15 14:49:24 -0700

    Update IDL Update setup script for kernel

--- sal_lsstlaunch.bash
+++ sal_lsstlaunch.bash
@@ -2,13 +2,17 @@
 CONFIG_FILE=$1
 source /opt/lsst/sal/salbldsteps.bash
 setup ts_xml -t current
+setup ts_idl -t current
 setup ts_sal -t current
 setup ts_salobj -t current
-setup ts_scriptqueue -t current
 setup ts_ATDome -t current
 setup ts_ATDomeTrajectory -t current
+setup TS_ATMCSSSimulator -t current
+setup ts_simactuators -t current
 setup ts_standardscripts -t current
+setup ts_scriptqueue -t current
 setup ts_externalscripts -t current
+
 setup display_firefly
 if [ -e ${HOME}/notebooks/.user_setups ]; then
     source ${HOME}/notebooks/.user_setups
