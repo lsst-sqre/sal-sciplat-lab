af244f20cb27d0e6e2bfb965f65b8572dfa451da
adam <athornton@gmail.com>
2020-06-17 16:54:37 -0700

    Update to xml 6.0.0

--- salbldsteps.bash
+++ salbldsteps.bash
@@ -1,9 +1,15 @@
 # This just saves us some repetition in SAL setup scripts
 source ${LOADSTACK}
 setup lsst_distrib
-PYTHON_BUILD_VERSION=$(find /opt/lsst/software/stack -name Python.h | head -1 \
-			   | xargs dirname | tr '/' ' ' | awk '{print $NF}' \
-			   | sed 's/python//')
+PYTHON_BUILD_VERSION=$(find /opt/lsst/software/stack -name Python.h \
+                        2>/dev/null | head -1 | xargs dirname | \
+                        xargs basename | sed 's/python//')
 export PYTHON_BUILD_VERSION
+pdir=$(dirname $(which python3))
+PYTHON_BUILD_LOCATION=${pdir:0:-4}
+export PYTHON_BUILD_LOCATION
+PYTHON_INCLUDE_DIR="${PYTHON_BUILD_LOCATION}/include"
+PYTHON_INCLUDE_DIR="${PYTHON_INCLUDE_DIR}/python${PYTHON_BUILD_VERSION}"
+export PYTHON_INCLUDE_DIR
 source ${LSST_SDK_INSTALL}/setup.env
 
