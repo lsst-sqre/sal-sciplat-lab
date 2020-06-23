10647298743d37e7cc771ad8b0fe4e81e87dae71
adam <athornton@gmail.com>
2020-06-23 16:07:32 -0700

    Accomodate new conda layout

--- salbldsteps.bash
+++ salbldsteps.bash
@@ -1,9 +1,14 @@
 # This just saves us some repetition in SAL setup scripts
 source ${LOADSTACK}
+unset LD_PRELOAD
 setup lsst_distrib
-PYTHON_BUILD_VERSION=$(find /opt/lsst/software/stack -name Python.h | head -1 \
-			   | xargs dirname | tr '/' ' ' | awk '{print $NF}' \
-			   | sed 's/python//')
-export PYTHON_BUILD_VERSION
+
+PYTHON_BUILD_VERSION=$(python3 --version|cut -f2 -d' '|cut -c 1-3)m
+p_dir=$(dirname $(command -v python3))
+export PYTHON_BUILD_LOCATION=${p_dir:0:-4}
+PYTHON_INCLUDE_DIR=${PYTHON_BUILD_LOCATION}/include
+PYTHON_INCLUDE_DIR="${PYTHON_INCLUDE_DIR}/python${PYTHON_BUILD_VERSION}"
+PYTHON_LIBRARY_DIR=${PYTHON_BUILD_LOCATION}/lib
+export PYTHON_LIBRARY_DIR PYTHON_INCLUDE_DIR PYTHON_BUILD_VERSION
 source ${LSST_SDK_INSTALL}/setup.env
 
